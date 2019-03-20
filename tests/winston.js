4e40d3086a5a467d5f6c4f007ebcaca166a72dee
Dmytro Pustovit <dipustovit@gmail.com>
2019-03-20 23:14:48 +0200

    basic tests added

---
+++ tests/winston.js
@@ -0,0 +1,31 @@
+import { createLogger, format } from 'winston';
+import Transport from 'winston-transport';
+import { assert } from 'chai';
+import transport from '../src';
+
+suite('Winston');
+test('Positive: check transport', function () {
+    const t = new transport();
+
+    assert.instanceOf(t, Transport);
+});
+test('Positive: createLogger', function () {
+    const logger = createLogger({
+        level  : 'debug',
+        levels : {
+            error   : 0,
+            warn    : 1,
+            info    : 2,
+            notice  : 3,
+            verbose : 4,
+            debug   : 5
+        },
+        format : format.combine(
+            format.timestamp(),
+            format.json()
+        ),
+        transports : [ new transport() ]
+    });
+
+    assert.exists(logger);
+});
