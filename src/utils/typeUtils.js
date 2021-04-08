24badd9e5d5234bdf8243771f41a18343c850c36
Dmytro Pustovit <dipustovit@gmail.com>
2021-04-08 23:16:25 +0300

    Chore: (refactor): removes unused functions

--- src/utils/typeUtils.js
+++ src/utils/typeUtils.js
@@ -1,24 +1,4 @@
 
-export function isString(x) {
-    return x && Object.prototype.toString.call(x) === '[object String]';
-}
-
-export function isClass(v) {
-    return typeof v === 'function' && /^\s*class\s+/.test(v.toString());
-}
-
 export function isFunction(x) {
     return x && Object.prototype.toString.call(x) === '[object Function]';
-}
-
-export function isEmpty(x) {
-    return x && x.constructor === Object && Object.keys(x).length === 0;
-}
-
-export function isObject(x) {
-    return x && Object.prototype.toString.call(x) === '[object Object]';
-}
-
-export function isArray(x) {
-    return x && Object.prototype.toString.call(x) === '[object Array]';
 }
